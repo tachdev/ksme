4ebe379755d86d7c471a389cbae60fdfc10a3ec7
saiman <rabbit_crew69@hotmail.com>
2015-01-16 23:55:41 +0700

    update ksme network map

--- app/views/group_region_sub.blade.php
+++ app/views/group_region_sub.blade.php
@@ -4,14 +4,18 @@
 <!--center side-->
 <div class="col-lg-12 no-padding" >
 	<div class="panel">
-			<header class="panel-heading">
-			    <div class="btn btn-success">
-			       Choose Ksme Region		        
-			    </div>
-			</header>
-			<div class="panel-body" style="text-align:center" >
+			<div class="panel-body" style="text-align:center" >				
 				<div class="col-lg-6">
-					<img src="assets/images/map_ksme.png" class="img-fullwidth">
+					<div style="text-align:left;">
+						<a href="<?php echo url('group_region'); ?>" class="btn btn-success">
+				       		Choose Ksme Region		        
+				    	</a>
+			    	</div>
+			    	<div class="ksme_region_logo">
+			    		<img src="assets/images/region/ksme_<?php echo Input::get('region'); ?>.png" >
+			    		<p><strong>5000 users</strong></p>
+			    	</div>
+					<img src="assets/images/map/<?php echo Input::get('region'); ?>_map.png" class="img-fullwidth">
 				</div>
 				<div class="panel col-lg-6 col-xs-12 no-padding gridbox" >
 					<header class="panel-heading bg-green">
@@ -28,7 +32,7 @@
 				                <div class="media-body region">
 				                    <h5 class="media-heading"><a href="#"><strong>Myname Sample 1</strong></a></h5>
 				                    <p>Kse Car Wash</p>
-				                	<p class="text-muted" style="font-size:20px;">
+				                	<p class="text-muted" style="font-size:16px;">
 							            <a href="#"><i class="fa fa-envelope"></i></a>
 							            <a href="#"><i class="fa fa-facebook-square"></i></a>
 							            <a href="#"><i class="fa fa-instagram"></i></a>
